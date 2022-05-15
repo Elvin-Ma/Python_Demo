6acd0fb9e5eb9284a8494b2415dcffbd69045a5e
mtn <tnma@birentech.com>
2022-05-15 20:07:57 +0800

    add format info

--- python_time_and_log/python_log_demo.py
+++ python_time_and_log/python_log_demo.py
@@ -1,5 +1,17 @@
 from python_log_looger import logger
-
+'''
+%(levelno)s : 打印日志级别的数值
+%(levelname)s : 打印日志级别的名称
+%(pathname)s : 打印当前执行程序的路径，其实就是sys.argv[0]
+%(filename)s : 打印当前执行程序名
+%(funcName)s : 打印日志的当前函数
+%(lineno)d : 打印日志的当前行号
+%(asctime)s : 打印日志的时间
+%(thread)d : 打印线程ID
+%(threadName)s : 打印线程名称
+%(process)d : 打印进程ID
+%(message)s : 打印日志信息
+'''
 try:
     a = input("请输入一个数字：（请一定输入数字）")
     logger.debug('用户输入的第一个值是：%s' % a)
